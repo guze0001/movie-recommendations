f325e274dd8743c217b7796e0613483362b7fe08
guze0001 <32398065+rguzel@users.noreply.github.com>
2017-12-18 22:28:53 -0500

    extra data fixed

--- js/main.js
+++ js/main.js
@@ -116,9 +116,12 @@ const app = {
                 h2.classList.add('movie-title');
 
                 let span = document.createElement('span');
-                span.textContent = "Date: " + movie.release_date + " Average: " + movie.vote_average ;//+ " Votes: " + movie.vote_count;
+                span.textContent = "Date: " + movie.release_date 
+                    let span2 = document.createElement('span');
+                span2.textContent = "Average: " + movie.vote_average ;//+ " Votes: " + movie.vote_count;
+                
                 span.classList.add('movie-date');
-
+                span2.classList.add('movie-date');
                 let p = document.createElement('p');
                 if (movie.overview == null || movie.overview.length < 1)
                     movie.overview = "No overview found.";
@@ -136,6 +139,7 @@ const app = {
                 div.appendChild(img);
                 div.appendChild(h2);
                 div.appendChild(span);
+                div.appendChild(span2);
                 div.appendChild(p);
                 div.addEventListener('click', app.getRecommended);
 
@@ -217,70 +221,3 @@ document.addEventListener('DOMContentLoaded', app.init);
 
 
 
-
-/*************
-        SAMPLE URLS
-        
-        1. To get the config data like image base urls
-        https://api.themoviedb.org/3/configuration?api_key=<APIKEY>
-        
-        2. To fetch a list of movies based on a keyword
-        https://api.themoviedb.org/3/search/movie?api_key=<APIKEY>&query=<keyword>
-        
-        3. To fetch more details about a movie
-        https://api.themoviedb.org/3/movie/<movie-id>?api_key=<APIKEY>
-        *************/
-//const APIKEY is inside key.js
-/*let baseURL = 'https://api.themoviedb.org/3/';
-let configData = null;
-let baseImageURL = null;
-
-let getConfig = function () {
-    let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
-    fetch(url)
-        .then((result) => {
-            return result.json();
-        })
-        .then((data) => {
-            baseImageURL = data.images.secure_base_url;
-            configData = data.images;
-            console.log('config:', data);
-            console.log('config fetched');
-            runSearch('jaws')
-        })
-        .catch(function (err) {
-            alert(err);
-        });
-}
-
-let runSearch = function (keyword) {
-    let url = ''.concat(baseURL, 'search/movie?api_key=', APIKEY, '&query=', keyword);
-    fetch(url)
-        .then(result => result.json())
-        .then((data) => {
-            //process the returned data
-            document.getElementById('output').innerHTML = JSON.stringify(data, null, 4);
-            //work with results array...
-
-        })
-}
-
-document.addEventListener('DOMContentLoaded', getConfig);*/
-/*******************************
-SAMPLE SEARCH RESULTS DATA
-{ "vote_count": 2762, 
-    "id": 578, 
-    "video": false, 
-    "vote_average": 7.5, 
-    "title": "Jaws", 
-    "popularity": 16.273358, 
-    "poster_path": "/l1yltvzILaZcx2jYvc5sEMkM7Eh.jpg", 
-    "original_language": "en", 
-    "original_title": "Jaws", 
-    "genre_ids": [ 27, 53, 12 ], 
-    "backdrop_path": "/slkPgAt1IQgxZXNrazEcOzhAK8f.jpg", 
-    "adult": false, 
-    "overview": "An insatiable great white shark terrorizes the townspeople of Amity Island, The police chief, an oceanographer and a grizzled shark hunter seek to destroy the bloodthirsty beast.", 
-    "release_date": "1975-06-18" 
-}
-*******************************/
